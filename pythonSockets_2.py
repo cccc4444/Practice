b2c3e6223d79343bc60d55535cab26f0c424bc90
Dan <KushlyanskyDan@outlook.com>
2020-01-22 15:45:24 +0200

    initial project version

---
+++ pythonSockets_2.py
@@ -0,0 +1,18 @@
+import socket
+
+host = socket.gethostname() 
+port = 5000  
+
+client_socket = socket.socket()  
+client_socket.connect((host, port))  
+
+message = input()  
+
+while True:
+        client_socket.send(message.encode())  
+        data = client_socket.recv(1024).decode()  
+        print('Received from server: ' + data)  
+        message = input()  
+client_socket.close()  
+
+
